36064a6af36c4c9e8591d56601e08b69992e2f95
Micah Chambers (eos) <mchambers@anduril.com>
2025-05-27 13:41:03 -0700

    readme

---
+++ egm96/build.rs
@@ -0,0 +1,49 @@
+use std::fs::File;
+use std::io::Write;
+
+const URL_ROOT: &str = "https://micahcc.github.io/egm96-rs/egm96/data";
+
+struct Fixture<'a> {
+    name: &'a str,
+    environ: &'a str,
+}
+
+fn load_blob(name: &str, env_name: &str, url: String, out_name: String) {
+    if let Ok(env) = std::env::var(env_name) {
+        std::os::unix::fs::symlink(env, out_name).expect("Failed to symlink");
+    } else {
+        let response = reqwest::blocking::get(url).expect(&format!("Failed to get {}", name));
+        let content = response.bytes().expect("failed to get bytes");
+        let mut dest =
+            File::create(&out_name).expect(&format!("Failed to create output file for {name}"));
+        dest.write_all(&content)
+            .expect(&format!("Failed to write to {}", out_name));
+    }
+}
+
+fn main() {
+    let fixtures = [
+        Fixture {
+            name: "egm96-15.png",
+            environ: "EGM96_15_MIN",
+        },
+        Fixture {
+            name: "egm96-5.png",
+            environ: "EGM96_5_MIN",
+        },
+    ];
+
+    for fixture in fixtures {
+        load_blob(
+            fixture.name,
+            fixture.environ,
+            format!("{URL_ROOT}/{}", fixture.name),
+            format!(
+                "{}/{}",
+                std::env::var("OUT_DIR").expect("no OUT_DIR"),
+                fixture.name
+            ),
+        );
+    }
+    //load_blob("egm96-5.png, "EGM96_5_MIN", format!("{URL_ROOT}/egm96-5.png"), "8ffac9c8de89190b58d4cde923743a332ca9f6f8b34c5d981bfca59172d9e782"));
+}
---
+++ egm96/data/README.md
@@ -0,0 +1,9 @@
+# NOTE: THIS IS A LOAD-BEARING DIRECTORY
+
+crates.io won't let me push my giant fixture PNGs, so I have to lazily load them
+in build.rs (or pull them from an environment variable in the case of nix).
+
+Github won't just serve files out of a git repository, so this page is made
+public through github pages so that the build.rs script can pull from it.
+
+So now egm96/build.rs references this directory.
