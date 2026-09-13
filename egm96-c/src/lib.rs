0348f23da1aecbe8f90ff250be8974559d00d8a8
Giorgio <64643719+JaskRendix@users.noreply.github.com>
2026-09-13 22:31:17 +0200

    refactor(ffi): remove redundant returns and clippy allowances (#21)

--- egm96-c/src/lib.rs
+++ egm96-c/src/lib.rs
@@ -1,5 +1,3 @@
-#![allow(clippy::needless_return)]
-
 /// Takes a lat, lon and returns the undulation, this is uses the most accurate
 /// method available, depending on build options.
 /// - If 5 arc-minute raster is available at build time it will use it
@@ -7,13 +5,13 @@
 /// - Otherwise will use spherical harmonic model that is slower but uses very little memory
 #[no_mangle]
 pub extern "C" fn egm96_altitude_offset(lat: f64, lon: f64) -> f64 {
-    return egm96::egm96_altitude_offset(lat, lon);
+    egm96::egm96_altitude_offset(lat, lon)
 }
 
 /// Takes a lat, lon and returns the undulation
 #[no_mangle]
 pub extern "C" fn egm96_compute_altitude_offset(lat: f64, lon: f64) -> f64 {
-    return egm96::egm96_compute_altitude_offset(lat, lon);
+    egm96::egm96_compute_altitude_offset(lat, lon)
 }
 
 /// Uses 15 arc-minute raster, lookup with interpolation
@@ -21,7 +19,7 @@ pub extern "C" fn egm96_compute_altitude_offset(lat: f64, lon: f64) -> f64 {
 #[allow(unused)]
 #[no_mangle]
 pub extern "C" fn egm96_raster_15_min_altitude_offset(lat: f64, lon: f64) -> f64 {
-    return egm96::egm96_raster_15_min_altitude_offset(lat, lon);
+    egm96::egm96_raster_15_min_altitude_offset(lat, lon)
 }
 
 /// Uses 5 arc-minute raster, lookup with interpolation
@@ -29,5 +27,5 @@ pub extern "C" fn egm96_raster_15_min_altitude_offset(lat: f64, lon: f64) -> f64
 #[cfg(feature = "raster_5_min")]
 #[no_mangle]
 pub extern "C" fn egm96_raster_5_min_altitude_offset(lat: f64, lon: f64) -> f64 {
-    return egm96::egm96_raster_5_min_altitude_offset(lat, lon);
+    egm96::egm96_raster_5_min_altitude_offset(lat, lon)
 }
--- egm96-static/src/lib.rs
+++ egm96-static/src/lib.rs
@@ -3,5 +3,5 @@
 /// Takes a lat, lon and returns the undulation
 #[no_mangle]
 pub extern "C" fn egm96_compute_altitude_offset(lat: f64, lon: f64) -> f64 {
-    return egm96::egm96_compute_altitude_offset(lat, lon);
+    egm96::egm96_compute_altitude_offset(lat, lon)
 }
